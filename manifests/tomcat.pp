4496f31cce56eab880a9546ce4c6c90fb586e386
Reid Vandewiele <reid@puppetlabs.com>
2013-04-04 21:13:09 -0700

    Add scope specification for globals

--- manifests/tomcat.pp
+++ manifests/tomcat.pp
@@ -2,12 +2,12 @@ class profile::tomcat {
 
   # Allow specification of a tomcat application server package name and version
   # based on a global variable (e.g. parameter passed in from the ENC)
-  $package = $profile_tomcat_package ? {
-    default => $profile_tomcat_package,
+  $package = $::profile_tomcat_package ? {
+    default => $::profile_tomcat_package,
     undef   => undef,
   }
-  $version = $profile_tomcat_version ? {
-    default => $profile_tomcat_version,
+  $version = $::profile_tomcat_version ? {
+    default => $::profile_tomcat_version,
     undef   => undef,
   }
 
