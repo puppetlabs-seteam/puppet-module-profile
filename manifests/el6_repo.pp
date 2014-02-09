b32ca3dc228e5be028d97468212f0a4f91454b49
Chris Barker <cbarker@puppetlabs.com>
2014-02-09 17:02:34 +1100

    setup system to use offline repository

--- manifests/el6_repo.pp
+++ manifests/el6_repo.pp
@@ -1,11 +1,32 @@
 class profile::el6_repo (
   $repohostname = 'master',
 ) {
-  yumrepo { 'se_mirror':
-    ensure   => present,
-    baseurl  => "http://${repohostname}/rpms/",
+  yumrepo { 'base':
+    descr      => 'CentOS-$releasever - Base',
+    gpgcheck   => '1',
+    enabled    => '0',
+    gpgkey     => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6',
+    mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=os',
+  }
+  yumrepo { 'extras':
+    descr      => 'CentOS-$releasever - Extras',
+    gpgcheck   => '1',
+    enabled    => '0',
+    gpgkey     => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6',
+    mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=extras',
+  }
+  yumrepo { 'updates':
+    descr      => 'CentOS-$releasever - Updates',
+    gpgcheck   => '1',
+    enabled    => '0',
+    gpgkey     => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6',
+    mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=updates',
+  }
+  yumrepo { 'se-repo':
+    baseurl  => 'http://master/rpms',
+    descr    => 'SE Cached Files',
     enabled  => 1,
-    gpgcheck => 0,
-    priority => 1,
+    gpgcheck => 1,
+    gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6',
   }
 }
