83df754ceb3229b4ed9381b2ef7012482ec7c397
Chris Barker <cbarker@puppetlabs.com>
2014-06-03 07:07:41 +1000

    Assumes jenkins is mirrored locally
    First pass at updating sample.war to work with new tomcat war module

---
+++ manifests/app/sample.pp
@@ -0,0 +1,14 @@
+# Class: tomcat::sample
+#
+#   This class installs the sample war in tomcat
+#
+class profile::app::sample(
+  $sample_version = '1.0',
+) {
+  $sample_war_file = "sample-${sample_version}.war"
+
+  tomcat::war { 'sample':
+    filename => $sample_war_file,
+    require  => Class['tomcat'],
+  }
+}
