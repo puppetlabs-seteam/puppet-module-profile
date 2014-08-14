992966dac86fa8005e6360224e2d72d00674ffbd
John Hong <obscured1975@gmail.com>
2014-08-14 10:33:32 -0400

    Added logic to check for razor_content_installed fact.

--- manifests/razor/demo.pp
+++ manifests/razor/demo.pp
@@ -1,83 +1,87 @@
 class puppet-module-profile::razor::demo {
 
-  file { '/tmp/pe-broker.json':
-    ensure => present,
-    source => 'puppet:///modules/puppet-module-profile/razordemo/pe-broker.json',
-  }
+  if $razor_content_installed != 'true' {
 
-  file { '/tmp/centos-6.5-repo.json':
-    ensure => present,
-    source => 'puppet:///modules/puppet-module-profile/razordemo/centos-6.5-repo.json',
-  }
+    file { '/tmp/pe-broker.json':
+      ensure => present,
+      source => 'puppet:///modules/puppet-module-profile/razordemo/pe-broker.json',
+    }
 
-  file { '/tmp/small-tag.json':
-    ensure => present,
-    source => 'puppet:///modules/puppet-module-profile/razordemo/small-tag.json',
-  }
+    file { '/tmp/centos-6.5-repo.json':
+      ensure => present,
+      source => 'puppet:///modules/puppet-module-profile/razordemo/centos-6.5-repo.json',
+    }
 
-  file { '/tmp/large-tag.json':
-    ensure => present,
-    source => 'puppet:///modules/puppet-module-profile/razordemo/large-tag.json',
-  }
+    file { '/tmp/small-tag.json':
+      ensure => present,
+      source => 'puppet:///modules/puppet-module-profile/razordemo/small-tag.json',
+    }
 
-  file { '/tmp/policy-small.json':
-    ensure => present,
-    source => 'puppet:///modules/puppet-module-profile/razordemo/policy-small.json',
-  }
+    file { '/tmp/large-tag.json':
+      ensure => present,
+      source => 'puppet:///modules/puppet-module-profile/razordemo/large-tag.json',
+    }
 
-  file { '/tmp/policy-large.json':
-    ensure => present,
-    source => 'puppet:///modules/puppet-module-profile/razordemo/policy-large.json',
-  }
+    file { '/tmp/policy-small.json':
+      ensure => present,
+      source => 'puppet:///modules/puppet-module-profile/razordemo/policy-small.json',
+    }
 
-  exec { 'Install Repo':
-    path => "/usr/bin",
-    command => "razor create-repo --json /tmp/centos-6.5-repo.json",
-    require => File["/tmp/centos-6.5-repo.json"],
-  } ->
+    file { '/tmp/policy-large.json':
+      ensure => present,
+      source => 'puppet:///modules/puppet-module-profile/razordemo/policy-large.json',
+    }
 
-  exec { 'Install PE Broker':
-    path => "/usr/bin",
-    command => "razor create-broker --json /tmp/pe-broker.json",
-    require => File["/tmp/pe-broker.json"],
-  } ->
+    exec { 'Install Repo':
+      path => "/usr/bin",
+      command => "razor create-repo --json /tmp/centos-6.5-repo.json",
+      require => File["/tmp/centos-6.5-repo.json"],
+    } ->
 
-  exec { 'Install Small Tag':
-    path => "/usr/bin",
-    command => "razor create-tag --json /tmp/small-tag.json",
-    require => File["/tmp/small-tag.json"],
-  } ->
+    exec { 'Install PE Broker':
+      path => "/usr/bin",
+      command => "razor create-broker --json /tmp/pe-broker.json",
+      require => File["/tmp/pe-broker.json"],
+    } ->
 
-  exec { 'Install Large Tag':
-    path => "/usr/bin",
-    command => "razor create-tag --json /tmp/large-tag.json",
-    require => File["/tmp/large-tag.json"],
-  } ->
+    exec { 'Install Small Tag':
+      path => "/usr/bin",
+      command => "razor create-tag --json /tmp/small-tag.json",
+      require => File["/tmp/small-tag.json"],
+    } ->
 
-  exec { 'Install Small Policy':
-    path => "/usr/bin",
-    command => "razor create-repo --json /tmp/policy-small.json",
-    require => File["/tmp/policy-small.json"],
-  } ->
+    exec { 'Install Large Tag':
+      path => "/usr/bin",
+      command => "razor create-tag --json /tmp/large-tag.json",
+      require => File["/tmp/large-tag.json"],
+    } ->
 
-  exec { 'Install Large Policy':
-    path => "/usr/bin",
-    command => "razor create-repo --json /tmp/policy-large.json",
-    require => File["/tmp/policy-large.json"],
-  } ->
+    exec { 'Install Small Policy':
+      path => "/usr/bin",
+      command => "razor create-repo --json /tmp/policy-small.json",
+      require => File["/tmp/policy-small.json"],
+    } ->
 
-  file { '/etc/puppetlabs/facter':
-    ensure => directory,
-  } ->
+    exec { 'Install Large Policy':
+      path => "/usr/bin",
+      command => "razor create-repo --json /tmp/policy-large.json",
+      require => File["/tmp/policy-large.json"],
+    } ->
 
-  file { '/etc/puppetlabs/facter/facts.d':
-    ensure => directory,
-  } ->
+    file { '/etc/puppetlabs/facter':
+      ensure => directory,
+    } ->
 
-  file { '/etc/puppetlabs/facter/facts.d/razor_content_installed.sh':
-    ensure => present,
-    mode => 0755,
-    source => 'puppet:///modules/puppet-module-profile/razordemo/razor_content_installed.sh',
+    file { '/etc/puppetlabs/facter/facts.d':
+      ensure => directory,
+    } ->
+
+    file { '/etc/puppetlabs/facter/facts.d/razor_content_installed.sh':
+      ensure => present,
+      mode => 0755,
+      source => 'puppet:///modules/puppet-module-profile/razordemo/razor_content_installed.sh',
+    }
+
   }
 
 }
