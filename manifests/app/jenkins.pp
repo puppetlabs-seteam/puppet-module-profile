ce479caccbc45d587041aefdaba5e851c75aefcf
James E. Jones <james.jones@puppetlabs.com>
2014-12-02 11:33:31 -0600

    modified jenkins.pp to include tomcat profile for idempotence

--- manifests/app/jenkins.pp
+++ manifests/app/jenkins.pp
@@ -1,30 +1,22 @@
 class profile::app::jenkins (
   $version = 'latest',
-  $tomcat_major_version = '7',
   $catalina_base = "/opt/apache-tomcat",
   $catalina_home = "${catalina_base}",
 ) {
   include profile::staging
-  class { 'java':
-    distribution => 'jre'
-  }
-  class { 'profile::tomcat':
-    version       => $tomcat_major_version,
-    catalina_base => $catalina_base,
-    catalina_home => $catalina_home,
-    instance_name => 'jenkins',
-  }->
+  include java
+  include profile::tomcat
   tomcat::setenv::entry { 'JENKINS_HOME':
-    value => "\"-DJENKINS_HOME=${catalina_base}/webapps/jenkins\"",
-    param => 'CATALINA_OPTS',
+    value  => "\"-DJENKINS_HOME=${catalina_base}/webapps/jenkins\"",
+    param  => 'CATALINA_OPTS',
     before => Tomcat::War [ "jenkins-${version}.war" ],
     notify => Tomcat::War [ "jenkins-${version}.war" ],
   }
   tomcat::war { "jenkins-${version}.war" :
-    war_source => "http://master.inf.puppetlabs.demo/war/${version}/jenkins.war",
+    war_source    => "http://master.inf.puppetlabs.demo/war/${version}/jenkins.war",
     catalina_base => "${catalina_base}",
-    before => File [ "${catalina_base}/webapps/jenkins" ],
-    notify => File [ "${catalina_base}/webapps/jenkins" ],
+    before        => File [ "${catalina_base}/webapps/jenkins" ],
+    notify        => File [ "${catalina_base}/webapps/jenkins" ],
   }
   file { "${catalina_base}/webapps/jenkins":
     ensure => 'link',
@@ -33,8 +25,8 @@ class profile::app::jenkins (
   tomcat::service { "jenkins":
     catalina_base => "${catalina_base}",
     catalina_home => "${catalina_home}",
-    service_name => "jenkins",
-    subscribe => File [ "${catalina_base}/webapps/jenkins" ],
+    service_name  => "jenkins",
+    subscribe     => File [ "${catalina_base}/webapps/jenkins" ],
   }
 }
 
--- manifests/tomcat.pp
+++ manifests/tomcat.pp
@@ -1,28 +1,25 @@
 class profile::tomcat (
-  $version = undef,
-  $catalina_base = undef,
-  $catalina_home = undef,
-  $instance_name = undef,
+  $version = '7',
+  $catalina_base = '/opt/apache-tomcat',
+  $catalina_home = '/opt/apache-tomcat',
+  $instance_name = 'default',
 ) {
   case $version {
      '6': { $tomcat_minor_version = '6.0.35' }
      '7': { $tomcat_minor_version = '7.0.25' }
   }
   include profile::firewall
-
-  class { '::tomcat':}
+  include ::tomcat
   tomcat::instance{ $instance_name:
-    install_from_source => true,
-    source_url => "http://master.inf.puppetlabs.demo/tomcat/apache-tomcat-${tomcat_minor_version}.tar.gz",
+    install_from_source    => true,
+    source_url             => "http://master.inf.puppetlabs.demo/tomcat/apache-tomcat-${tomcat_minor_version}.tar.gz",
     source_strip_first_dir => false,
-    catalina_base => "${catalina_base}",
-    catalina_home => "${catalina_home}",
+    catalina_base          => "${catalina_base}",
+    catalina_home          => "${catalina_home}",
   }
-
   firewall { '100 allow connections to tomcat':
     proto   => 'tcp',
     dport   => '8080',
     action  => 'accept',
   }
-
 }
