6a13b8f71710a600e1701dc2a5e1ce43c0c19f84
Reid Vandewiele <reid@puppetlabs.com>
2015-05-20 09:34:27 -0700

    Add simple Windows registry example
    The new/old profile::example::registry class is taken from code in the
    https://github.com/puppetlabs/saleseng-demo-environment/tree/master/modules/profile
    repository. This was an old version of the seteam/profile module, prior
    to it being split into a separate repository.
    
    The registry example can be used to demonstrate idempotency on Windows
    systems by creating registry keys if they don't exist, and enforcing
    their state and content.

---
+++ manifests/example/registry.pp
@@ -0,0 +1,24 @@
+class profile::example::registry {
+
+  registry_key { 'HKEY_LOCAL_MACHINE\Software\Demonstration':
+    ensure       => present,
+    purge_values => true,
+  }
+
+  registry_value { 'HKEY_LOCAL_MACHINE\Software\Demonstration\value1':
+    type => string,
+    data => 'this is a value',
+  }
+
+  registry_value { 'HKEY_LOCAL_MACHINE\Software\Demonstration\value2':
+    type         => dword,
+    data         => '0xFFFFFFFF',
+  }
+
+  registry_value { 'HKLM\Software\Demonstration\value3':
+    ensure => present,
+    type   => array,
+    data   => [ 0, 'zero', '0', 123456, 'two' ],
+  }
+
+}
